c12943f0f5a0d68686dc53dd42892e5d7dd73c9d
DiorSOS <rusramazan205@gmail.com>
2026-02-26 16:56:50 +0600

    choices

--- tabel_project/tabel_app/models.py
+++ tabel_project/tabel_app/models.py
@@ -9,7 +9,10 @@ class UserProfile(AbstractUser):
     ('ADMIN', 'ADMIN'),
     ('MENTOR', 'MENTOR'),
     ('STUDENT', 'STUDENT'))
+    role_user = models.CharField(max_length=10, choices=UserRole, default='ADMIN')
 
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name} , {self.role_user}'
 
 class MentorProfile(UserProfile):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
@@ -18,6 +21,11 @@ class MentorProfile(UserProfile):
     ('AI', 'AI'),
     ('FullStack', 'FullStack'),
     ('Frontend', 'Frontend'))
+    role_mentor = models.CharField(max_length=10, choices=SubjectChoices, default='Python')
+
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}, {self.role_mentor}'
+
 
 
 class StudentProfile(UserProfile):
@@ -26,25 +34,29 @@ class StudentProfile(UserProfile):
     parent_phone = PhoneNumberField()
 
 
+
 class Group(models.Model):
     course_name = models.CharField(max_length=100)
     duration = models.DateField()
     mentor = models.ForeignKey(MentorProfile, on_delete=models.CASCADE)
     study_days = models.DateField()
 
+
 class Lesson(models.Model):
     data = models.DateTimeField(auto_now=True)
-
 
     def __str__(self):
         return self.data
 
+
 class LessonRecord(models.Model):
     student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    grade = models.IntegerField(choices=[(i, str(i))for i in range(1,5)])
+    grade = models.IntegerField(choices=[(i, str(i))for i in range(2,5)])
     Attendance = (
         ('Present', 'Present'),
         ('Absent', 'Absent')
     )
-    attendance = models.CharField(max_length=1, choices=Attendance)
+    attendance = models.CharField(max_length=77, choices=Attendance)
+
+
