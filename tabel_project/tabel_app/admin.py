1eeb072569f59c74a006f279e9f11704a909c1f6
kazbekovetolkunai10-sketch <kazbekovatolkunai10@gmail.com>
2026-02-27 15:09:44 +0600

    view

--- tabel_project/tabel_app/admin.py
+++ tabel_project/tabel_app/admin.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 from django.contrib import admin
 from .models import UserProfile, MentorProfile, StudentProfile, Group, Lesson, LessonRecord
 
@@ -9,4 +7,4 @@ admin.site.register(StudentProfile)
 admin.site.register(Group)
 admin.site.register(Lesson)
 admin.site.register(LessonRecord)
->>>>>>> 5902db66d011ba7b07d852b65df7c164d4a043b5
+
